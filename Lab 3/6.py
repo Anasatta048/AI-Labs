31a25a458f0fcf07923432a9e621fcff5fb31470
Anasatta048 <145205987+Anasatta048@users.noreply.github.com>
2024-08-05 20:48:10 +0500

    Update 6.py

--- Lab 3/6.py
+++ Lab 3/6.py
@@ -1,3 +1,10 @@
-txt=input("Enter a string: ")
-new=txt[::-1]
-print(new)
+numbers=[1,2,3,4,5,6,7,8,9]
+odd=0
+even=0
+for x in numbers:
+    if x%2==0:
+        even+=1
+    else:
+        odd+=1
+print("Even: ",even)
+print("Odd: ",odd)
